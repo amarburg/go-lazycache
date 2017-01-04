805002dc0ada0ab5b4bbf2f4851ae0dd7b46fb1f
Aaron Marburg <amarburg@apl.washington.edu>
2017-01-03 16:47:04 -0800

    Very cursory JSON output.

--- lazycache.go
+++ lazycache.go
@@ -3,12 +3,15 @@ package main
 import (
   "fmt"
   "net/http"
+  "regexp"
+  "encoding/json"
 )
 import "github.com/amarburg/go-lazyfs"
 
 var OOIRawDataRootURL = "https://rawdata.oceanobservatories.org/"
 
 var fs *lazyfs.HttpFSSource = nil
+var trailingSlash = regexp.MustCompile(`/$`)
 
 func index(w http.ResponseWriter, req *http.Request) {
   fmt.Fprintf(w, "<a href=\"rawdata.oceanobservatories.org/\">rawdata.oceanobservatories.org/</a>\n")
@@ -19,8 +22,12 @@ func rawData( w http.ResponseWriter, req *http.Request ) {
   listing,err := fs.ReadHttpDir( req.URL.Path )
 
   if err == nil {
-    for _,child := range listing.Children {
-      fmt.Fprintf(w, "<a href=\"%s\">%s</a><br>\n", child, child )
+      for _,child := range listing.Children {
+        if( trailingSlash.MatchString( child ) ) {
+        fmt.Fprintf(w, "<a href=\"%s\">%s</a><br>\n", child, child )
+      } else {
+        fmt.Fprintf(w, "%s\n", child )
+      }
     }
 
     fmt.Fprintf(w, "\n")
@@ -28,21 +35,55 @@ func rawData( w http.ResponseWriter, req *http.Request ) {
     fmt.Fprintf( w, "Error: %s\n", err.Error() )
   }
 
-  }
+}
 
 
-  func main() {
+func rawDataJson( w http.ResponseWriter, req *http.Request ) {
+  listing,err := fs.ReadHttpDir( req.URL.Path )
 
-    var err error
-    fs, err = lazyfs.OpenHttpFSSource( OOIRawDataRootURL )
+  if err == nil {
 
-    if err != nil {
-      panic( fmt.Sprintf("Error opening HTTP FS Source: %s", err.Error() ) )
+    type DirListing struct {
+      Directories []string
+      Files []string
     }
 
-    fmt.Printf("Starting http handler at http://localhost:5000/")
+    output := DirListing{}
 
-    http.HandleFunc("/", rawData)
-    //http.HandleFunc("/", index)
-    http.ListenAndServe("127.0.0.1:5000", nil)
+    for _,child := range listing.Children {
+      if( trailingSlash.MatchString( child ) ) {
+        output.Directories = append(output.Directories, child )
+      } else {
+        output.Files = append(output.Files, child )
+      }
+    }
+
+
+    b, err := json.MarshalIndent(output,"","  ")
+    if err != nil {
+      fmt.Fprintln(w, "JSON error:", err)
+    }
+
+    w.Write(b)
+
+  } else {
+    fmt.Fprintf( w, "Error: %s\n", err.Error() )
   }
+
+}
+
+func main() {
+
+  var err error
+  fs, err = lazyfs.OpenHttpFSSource( OOIRawDataRootURL )
+
+  if err != nil {
+    panic( fmt.Sprintf("Error opening HTTP FS Source: %s", err.Error() ) )
+  }
+
+  fmt.Printf("Starting http handler at http://localhost:5000/\n")
+
+  http.HandleFunc("/", rawDataJson)
+  //http.HandleFunc("/", index)
+  http.ListenAndServe("127.0.0.1:5000", nil)
+}
