ffa6b78ee627c98bcbf3dea99e3edbb8b9ec877f
Aaron Marburg <amarburg@apl.washington.edu>
2017-01-12 20:45:49 -0800

    Host handler working..

--- host_handler.go
+++ host_handler.go
@@ -5,33 +5,34 @@ import "net/http"
 import "strings"
 
 type HandlerCommon struct {
-  root string   // For simplicity, root is stored without the trailing slash
+//  root string   // For simplicity, root is stored without the trailing slash
   fs *HttpFS
 }
 
 type Node struct {
-  path, fullPath    string
+  path, trimPath    string
   common *HandlerCommon
   children map[string]*Node
 }
 
-type RootNode struct {
-  nodes    *Node
-  rootPath string
-}
+// type RootNode struct {
+//   nodes    *Node
+//   rootPath string
+// }
 
 
 
-func (node *Node) Handle( path []string, w http.ResponseWriter, req *http.Request ) {
+func (node *Node) handle( path []string, w http.ResponseWriter, req *http.Request ) {
   fmt.Fprintf( w, "Node: %s\n", node.path )
 
   if( len( path ) > 0 ) {
     child,ok := node.children[ path[0] ]
     if ok  {
-      child.Handle( path[1:], w, req )
+      child.handle( path[1:], w, req )
     } else {
-      node.children[ path[0] ] = node.makeNode( path )
-      node.children[ path[0] ].Handle( path[1:], w, req )
+      newNode := node.makeNode( path )
+      node.children[ path[0] ] = newNode
+      newNode.handle( path[1:], w, req )
     }
   }
 }
@@ -39,11 +40,11 @@ func (node *Node) Handle( path []string, w http.ResponseWriter, req *http.Reques
 func (handle *Node) makeNode( path []string ) (*Node) {
   fmt.Println("Creating node for", path[0] )
 
-  fullPath := handle.fullPath + path[0] + "/"
+  fullPath := handle.trimPath + path[0] + "/"
   node := Node{ common: handle.common,
                 children: make( map[string]*Node ),
                 path: path[0] + "/",
-                fullPath: fullPath }
+                trimPath: fullPath }
 
   fmt.Println("registering ", fullPath )
   http.Handle( fullPath, node )
@@ -52,38 +53,34 @@ func (handle *Node) makeNode( path []string ) (*Node) {
 }
 
 func (node Node) ServeHTTP( w http.ResponseWriter, req *http.Request ) {
-  fmt.Printf("Default handler %s\n", req.URL.Path )
+  fmt.Printf("Node handler %s for %s\n", node.path, req.URL.Path )
 
-  shortPath := strings.TrimPrefix( req.URL.Path, node.path )
+  shortPath := strings.TrimPrefix( req.URL.Path, node.trimPath )
   elements  := strings.Split( shortPath, "/" )
   elements  = elements[:len(elements)-1]
+
   // Starting root, pass off to handlers
-
-  node.Handle( elements, w, req )
+  node.handle( elements, w, req )
 }
 
 
-func MakeRootNode( fs *HttpFS, root string ) (*RootNode) {
-  return &RootNode{
-              rootPath: root,
-              nodes:  &Node{path: "/",
-                          fullPath: "/",
-                          children: make( map[string]*Node ),
-                          common: &HandlerCommon{ fs: fs,
-                                                  root: strings.TrimRight(root,"/"), },
-                            },
-  }
+func MakeRootNode( fs *HttpFS, root string ) (*Node) {
+  return &Node{path: "/",
+                trimPath: root,
+                children: make( map[string]*Node ),
+                common: &HandlerCommon{ fs: fs },
+              }
 }
 
 
 
-func (root RootNode) ServeHTTP( w http.ResponseWriter, req *http.Request ) {
-  fmt.Printf("Default handler %s\n", req.URL.Path )
-
-  shortPath := strings.TrimPrefix( req.URL.Path, root.rootPath )
-  elements  := strings.Split( shortPath, "/" )
-  elements  = elements[:len(elements)-1]
-  // Starting root, pass off to handlers
-
-  root.nodes.Handle( elements, w, req )
-}
+// func (root RootNode) ServeHTTP( w http.ResponseWriter, req *http.Request ) {
+//   fmt.Printf("Default handler %s\n", req.URL.Path )
+//
+//   shortPath := strings.TrimPrefix( req.URL.Path, root.rootPath )
+//   elements  := strings.Split( shortPath, "/" )
+//   elements  = elements[:len(elements)-1]
+//   // Starting root, pass off to handlers
+//
+//   root.nodes.Handle( elements, w, req )
+// }
