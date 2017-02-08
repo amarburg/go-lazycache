6e7da7fa56b91b04e2efbce1f31f19a2824e9594
Aaron Marburg <amarburg@apl.washington.edu>
2017-02-08 10:29:08 -0800

    Moved common Regexps to their own file.

---
+++ regexp.go
@@ -0,0 +1,7 @@
+package main
+
+import "regexp"
+
+var movExtension = regexp.MustCompile(`\.mov$`)
+var mp4Extension = regexp.MustCompile(`\.mp4$`)
+var jsonExtension = regexp.MustCompile(`\.json$`)
