497d32c65d024dcecb82015fc301f61842529399
Aaron Marburg <amarburg@apl.washington.edu>
2017-06-20 10:29:29 -0700

    Removed default_handler, it isn't doing anything anymore.

--- default_handler.go
+++
@@ -1,8 +0,0 @@
-package lazycache
-
-import "fmt"
-import "net/http"
-
-func HandleDefault(node *Node, path []string, w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Default handler: %s\n", node.Path)
-}
